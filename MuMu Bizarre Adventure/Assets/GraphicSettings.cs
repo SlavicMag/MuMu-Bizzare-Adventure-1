3c89934a443f48f0bdb0265fc2292883e5c3e1b6
SlavicMag <govoruxadaniil@gmail.com>
2026-01-21 19:46:20 +0300

    j
    jj

--- MuMu Bizarre Adventure/Assets/GraphicSettings.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class GraphicSettings : MonoBehaviour
-{
-    public void SetQuality(int qualityIndex)
-    {
-        QualitySettings.SetQualityLevel(qualityIndex);
-    }
-}
--- MuMu Bizarre Adventure/Assets/Skripts/inventory/DropItems.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DropItems : MonoBehaviour
-{
-    //eto dla save
-}
